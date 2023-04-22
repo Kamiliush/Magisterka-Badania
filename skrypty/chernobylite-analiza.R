88e081c6ee75d3978caff2f4c68563851fa65199
Kamil <92521451+Kamiliush@users.noreply.github.com>
2023-04-22 18:52:54 +0200

    Graphs

--- skrypty/chernobylite-analiza.R
+++ skrypty/chernobylite-analiza.R
@@ -1,10 +1,48 @@
-daneGry <- read.csv(file = 'C:/Users/Kamil/Desktop/chernobylite-bazowy.csv',sep = ';')
-head(daneGry)
+install.packages("dplyr")
+install.packages("ggplot2")
+library(dplyr)
+library(ggplot2)
 
 chernobyliteBase <- read.csv(file = './dane/chernobylite/bazowy.csv',sep = ';')
-chernobyliteDLSSQuality <- read.csv(file = './dane/chernobylite/bazowy.csv',sep = ';')
-chernobyliteDLSSBalance <- read.csv(file = './dane/chernobylite/bazowy.csv',sep = ';')
-chernobyliteDLSSPerformance <- read.csv(file = './dane/chernobylite/bazowy.csv',sep = ';')
-chernobyliteFSRQuality <- read.csv(file = './dane/chernobylite/bazowy.csv',sep = ';')
-chernobyliteFSRBalance <- read.csv(file = './dane/chernobylite/bazowy.csv',sep = ';')
-chernobyliteFSRPerformance <- read.csv(file = './dane/chernobylite/bazowy.csv',sep = ';')
+chernobyliteDLSSQuality <- read.csv(file = './dane/chernobylite/dlss-jakosc.csv',sep = ';')
+chernobyliteDLSSBalance <- read.csv(file = './dane/chernobylite/dlss-balans.csv',sep = ';')
+chernobyliteDLSSPerformance <- read.csv(file = './dane/chernobylite/dlss-jakosc.csv',sep = ';')
+chernobyliteFSRQuality <- read.csv(file = './dane/chernobylite/fsr-jakosc.csv',sep = ';')
+chernobyliteFSRBalance <- read.csv(file = './dane/chernobylite/fsr-balans.csv',sep = ';')
+chernobyliteFSRPerformance <- read.csv(file = './dane/chernobylite/fsr-wydajnosc.csv',sep = ';')
+
+"Datetime GPU.temperature GPU.usage FB.usage Memory.usage Core.clock Power 
+CPU.temperature CPU.usage CPU.clock RAM.usage Framerate Frametime Framerate.Avg"
+
+
+chernobyliteBase$Dataset <- "Base"
+chernobyliteDLSSQuality$Dataset <- "DLSS Quality"
+chernobyliteDLSSBalance$Dataset <- "DLSS Balance"
+chernobyliteDLSSPerformance$Dataset <- "DLSS Performance"
+chernobyliteFSRQuality$Dataset <- "FSR Quality"
+chernobyliteFSRBalance$Dataset <- "FSR Balance"
+chernobyliteFSRPerformance$Dataset <- "FSR Performance"
+
+combined_data <- rbind(chernobyliteBase, chernobyliteDLSSQuality, chernobyliteDLSSBalance, chernobyliteDLSSPerformance,
+                       chernobyliteFSRQuality, chernobyliteFSRBalance, chernobyliteFSRPerformance)
+
+# write.csv(combined_data, file = "combined_data.csv", row.names = FALSE, sep = ';')
+write.table(combined_data, file = "combined_data.csv", row.names = FALSE, dec = ".", sep = ";", quote = FALSE)
+
+ggplot(data = combined_data, aes(x = Datetime, y = Framerate, color = Dataset)) +
+  geom_line() +
+  labs(title = "Framerate Comparison",
+       x = "Datetime",
+       y = "Framerate") +
+  theme_minimal() +
+  scale_color_manual(values = c("Base" = "blue", 
+                                "DLSS Quality" = "red", 
+                                "DLSS Balance" = "green", 
+                                "DLSS Performance" = "purple",
+                                "FSR Quality" = "orange",
+                                "FSR Balance" = "brown",
+                                "FSR Performance" = "pink")) +
+  theme(legend.title = element_blank())
+
+
+
---
+++ skrypty/farming-simulator-analiza.R
@@ -0,0 +1,10 @@
+
+
+farmingSimulatorBase <- read.csv(file = './dane/farming-simulator/bazowy.csv',sep = ';')
+farmingSimulatorDLSSQuality <- read.csv(file = './dane/farming-simulator/dlss-jakosc.csv',sep = ';')
+farmingSimulatorDLSSBalance <- read.csv(file = './dane/farming-simulator/dlss-balans.csv',sep = ';')
+farmingSimulatorDLSSPerformance <- read.csv(file = './dane/farming-simulator/dlss-jakosc.csv',sep = ';')
+farmingSimulatorFSRQuality <- read.csv(file = './dane/farming-simulator/fsr-jakosc.csv',sep = ';')
+farmingSimulatorFSRBalance <- read.csv(file = './dane/farming-simulator/fsr-balans.csv',sep = ';')
+farmingSimulatorFSRPerformance <- read.csv(file = './dane/farming-simulator/fsr-wydajnosc.csv',sep = ';')
+
