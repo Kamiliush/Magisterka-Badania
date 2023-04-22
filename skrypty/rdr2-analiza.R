5126676cef27a02817dfaa7c5b1320a4478f6719
pawel.wojcik7 <pawel.wojcik7@pollub.edu.pl>
2023-04-22 18:28:52 +0200

    asd

---
+++ skrypty/rdr2-analiza.R
@@ -0,0 +1,8 @@
+rdrBase <- read.csv(file = './dane/rdr2/bazowy.csv',sep = ';')
+rdrDLSSQuality <- read.csv(file = './dane/rdr2/bazowy.csv',sep = ';')
+rdrDLSSBalance <- read.csv(file = './dane/rdr2/bazowy.csv',sep = ';')
+rdrDLSSPerformance <- read.csv(file = './dane/rdr2/bazowy.csv',sep = ';')
+rdrFSRQuality <- read.csv(file = './dane/rdr2/bazowy.csv',sep = ';')
+rdrFSRBalance <- read.csv(file = './dane/rdr2/bazowy.csv',sep = ';')
+rdrFSRPerformance <- read.csv(file = './dane/rdr2/bazowy.csv',sep = ';')
+
